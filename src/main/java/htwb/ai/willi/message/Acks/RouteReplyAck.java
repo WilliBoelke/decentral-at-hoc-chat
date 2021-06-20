3b44f86fbce5e4d2823c667e9649811c3ca28b3c
willi <williboelke@outlook.com>
2021-06-20 17:48:21 +0200

    Implemented SendTextRequestAck

--- src/main/java/htwb/ai/willi/message/Acks/RouteReplyAck.java
+++ src/main/java/htwb/ai/willi/message/Acks/RouteReplyAck.java
@@ -1,15 +1,18 @@
 package htwb.ai.willi.message.Acks;
 
+import htwb.ai.willi.controller.Controller;
 import htwb.ai.willi.message.Request;
 import htwb.ai.willi.message.RouteReply;
 
 import java.io.ByteArrayOutputStream;
 import java.nio.charset.StandardCharsets;
+import java.util.logging.Logger;
 
 
 public class RouteReplyAck extends Request
 {
 
+     public static final Logger LOG = Logger.getLogger(Controller.class.getName());
 
      public RouteReplyAck()
      {
@@ -19,6 +22,7 @@ public class RouteReplyAck extends Request
      public static RouteReplyAck getInstanceFromEncodedString(String encoded, String address)
      {
           byte addressAsByte = Byte.parseByte(address.substring(2));
+          LOG.info("REPLY ADDRESS ====== " +addressAsByte);
           RouteReplyAck reply = new RouteReplyAck();
           reply.setLastHopInRoute(addressAsByte);
           return reply;
