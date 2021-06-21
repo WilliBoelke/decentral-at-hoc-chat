db14a478161a2de3738d42328ebdbaf42240c40a
willi <williboelke@outlook.com>
2021-06-21 20:37:01 +0200

    Delete Route on RouteError

--- src/main/java/htwb/ai/willi/router/RouteErrorRouter.java
+++ src/main/java/htwb/ai/willi/router/RouteErrorRouter.java
@@ -1,6 +1,8 @@
 package htwb.ai.willi.router;
 
 import htwb.ai.willi.message.Request;
+import htwb.ai.willi.message.RouteError;
+import htwb.ai.willi.routing.RoutingTable;
 
 public class RouteErrorRouter extends Router
 {
@@ -8,7 +10,15 @@ public class RouteErrorRouter extends Router
      @Override
      protected void anyCase(Request request)
      {
+          RouteError routeError = (RouteError) request;
+          byte address = routeError.getUnreachableDestinationAddress();
+          byte sequenceNumber = routeError.getUnreachableDestinationSequenceNumber();
 
+          RoutingTable.Route routeToUnreachable = RoutingTable.getInstance().getRouteTo(address);
+          if (routeToUnreachable.getDestinationSequenceNumber() < sequenceNumber)
+          {
+               RoutingTable.getInstance().removeRoute(address);
+          }
      }
 
      @Override
