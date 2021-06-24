2d44ace4245457e71e3a9102276d2a4c4a8f1d22
willi <williboelke@outlook.com>
2021-06-24 16:51:21 +0200

    Working with events for aAT.Ok and AT.SENDED

--- src/main/java/htwb/ai/willi/loraModule/ModuleManger/LoraModule.java
+++ src/main/java/htwb/ai/willi/loraModule/ModuleManger/LoraModule.java
@@ -88,7 +88,7 @@ public class LoraModule implements PropertyChangeListener
      {
           Process process = null;
           try{
-               process = Runtime.getRuntime().exec(new String[]{"reasset"});
+               process = Runtime.getRuntime().exec(new String[]{"reset"});
           }catch(Exception e) {
                System.out.println("Exception Raised" + e.toString());
           }
